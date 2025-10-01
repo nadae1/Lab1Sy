0244e636e1afcdd027bb8120481c1ca322b44072
nadae1 <bourchadanada53@gmail.com>
2025-10-01 04:41:20 +0100

    Initial commit

---
+++ website-skeleton/src/Controller/EtudiantController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EtudiantController extends AbstractController
+{
+    /**
+     *@Route("/etudiant", name="etudiant")
+     */
+
+    public function index(): Response
+    {
+        return new Response('Bonjour mes etudiantss :) ');
+    }
+
+    /**
+     *@Route("/etudiant/{id}", name="afficher_etudiant" , requirements={"id"="\d{2}"})
+     */
+    public function afficherEtudiant($id): Response {
+        return new Response("Bonjour l'etudiant  numero ".$id);
+    }
+
+
+    /**
+     * @Route("/etudiant/voir-name/{name}", name="voir_etudiant")
+     */
+
+    public function voirName($name): Response
+    {
+        return $this->render('etudiant/etudiant.html.twig', [
+            'name' => $name,
+        ]);
+    }
+}
